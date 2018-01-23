9c44d6ca4cd0986bc620d309c90ea3660279c5bf
pps <ledevzl@gmail.com>
2018-01-23 16:01:15 +0000

    mintable token

--- contracts/OurTutoCoin.sol
+++ contracts/OurTutoCoin.sol
@@ -76,6 +76,56 @@ contract OurTutoCoin {
 		return true;
 	}
 
+	/**
+	   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+	   *
+	   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+	   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+	   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+	   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+	   * @param _spender The address which will spend the funds.
+	   * @param _value The amount of tokens to be spent.
+	   */
+	function approve(address _spender, uint256 _value) public returns (bool) {
+		allowed[msg.sender][_spender] = _value;
+		Approval(msg.sender, _spender, _value);
+		return true;
+	}
+
+	/**
+	* @dev Transfer tokens from one address to another
+	* @param _from address The address which you want to send tokens from
+	* @param _to address The address which you want to transfer to
+	* @param _value uint256 the amount of tokens to be transferred
+	*/
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+		require(_to != address(0));
+		require(_value <= balances[_from]);
+		require(_value <= allowed[_from][msg.sender]);
+
+		balances[_from] = balances[_from].sub(_value);
+		balances[_to] = balances[_to].add(_value);
+		allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+		Transfer(_from, _to, _value);
+		return true;
+	}
 
 
+	/**
+	* @dev Allows the current owner to transfer control of the contract to a newOwner.
+	* @param newOwner The address to transfer ownership to.
+	*/
+	function transferOwnership(address newOwner) public onlyOwner {
+		require(newOwner != address(0));
+		OwnershipTransferred(owner, newOwner);
+		owner = newOwner;
+	}
+
+	function mint(address _to, uint256 _amount) onlyOwner public returns (bool) {
+		totalSupply = totalSupply.add(_amount);
+		balances[_to] = balances[_to].add(_amount);
+		Mint(_to, _amount);
+		Transfer(address(0), _to, _amount);
+		return true;
+	}
 }
