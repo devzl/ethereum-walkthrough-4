4f2a37b0639ed68ab8f9a47e551f6d62dbe93ef7
pps <ledevzl@gmail.com>
2018-01-28 22:07:56 +0000

    ERC20 & ERC721 single file implementations

--- contracts/OurTutoCoin.sol
+++ contracts/OurTutoCoin.sol
@@ -1,9 +1,49 @@
 pragma solidity ^0.4.18;
 
-import './SafeMath.sol';
+    /**
+     * @title SafeMath
+     * @dev Math operations with safety checks that throw on error
+     * @notice source: https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/contracts/math/SafeMath.sol
+     */
+
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+	/**
+	 * @title OurTutoCoin
+	 * @dev A one file ERC20 token for the 4th part of the Ethereum Development Walkthrough tutorial series
+	 * 
+	 * @notice check https://github.com/OpenZeppelin/zeppelin-solidity for a better, modular code
+	*/
 
 contract OurTutoCoin {
-	using SafeMath for uint256;
+	using SafeMath for uint256; // SafeMath methods will be available for the type "unit256"
 
 	uint256 public totalSupply;
 
